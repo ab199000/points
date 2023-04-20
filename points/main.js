bc9bd77d2fbcd13bbe3f19b68da6508e9bef3b7a
ab199000 <97779753+ab199000@users.noreply.github.com>
2023-04-20 13:20:27 +0300

    Add files via upload

--- points/main.js
+++ points/main.js
@@ -1,107 +1,294 @@
-const body = document.querySelector('.field')
+const body = document.querySelector(".field");
 
-let motion = 0
+let motion = 0;
 
-let field = [[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[]]
 
-for(let i = 0;i<25;i++){
-    let line = document.createElement("div")
-    line.classList.add(".line")
-    for(let j = 0; j <25;j++){
-        let btn = document.createElement('button')
-        btn.id = `${i}n${j}`
-        line.append(btn)
-        body.append(line)
-    }
-}
+let massPaints = []
+let field = [
+  [],
+  [],
+  [],
+  [],
+  [],
+  [],
+  [],
+  [],
+  [],
+  [],
+  [],
+  [],
+  [],
+  [],
+  [],
+  [],
+  [],
+  [],
+  [],
+  [],
+  [],
+  [],
+  [],
+  [],
+  [],
+];
 
-body.addEventListener("click",(event)=>{
-    let point = event.target
-    if(point.tagName != "BUTTON"){
-        return;
-    }
-    putPoint (point)
-    checkPoints(coordinatesPoint(point).str,coordinatesPoint(point).col,motion)
-    check(coordinatesPoint(point).str,coordinatesPoint(point).col,motion)
-})
+// for (let i = 0; i < 25; i++) {
+//   let line = document.createElement("div");
+//   line.classList.add(".line");
+//   for (let j = 0; j < 25; j++) {
+//     let btn = document.createElement("button");
+//     btn.id = `${i}n${j}`;
+//     line.append(btn);
+//     body.append(line);
+//   }
+// }
 
-function putPoint (point) {
-    if(point.classList.value != ''){
-        return
-    }
-    if(motion){
-        point.classList.add("secondPlayer")
-        // field[coordinatesPoint(point).str][coordinatesPoint(point).col] = motion
-        field2[coordinatesPoint(point).str][coordinatesPoint(point).col].player = motion
-        motion = 0
-        return;
-    }
-    point.classList.add("firstPlayer")
+body.addEventListener("click", (event) => {
+  let point = event.target;
+  if (point.tagName != "BUTTON") {
+    return;
+  }
+  putPoint(point);
+  console.log(motion)
+  
+});
+
+function putPoint(point) {
+  if (point.classList.value != "") {
+    return;
+  }
+  if (motion) {
+    point.classList.add("secondPlayer");
     // field[coordinatesPoint(point).str][coordinatesPoint(point).col] = motion
-    field2[coordinatesPoint(point).str][coordinatesPoint(point).col].player = motion
-    motion = 1
+    field2[coordinatesPoint(point).str][coordinatesPoint(point).col].player =
+      motion;
+    giveAllPaints(coordinatesPoint(point).str, coordinatesPoint(point).col, motion)
+    motion = 0;
+    console.log(massPaints)
+    return;
+  }
+  point.classList.add("firstPlayer");
+  // field[coordinatesPoint(point).str][coordinatesPoint(point).col] = motion
+  field2[coordinatesPoint(point).str][coordinatesPoint(point).col].player =
+    motion;
+    giveAllPaints(coordinatesPoint(point).str, coordinatesPoint(point).col, motion)
+  motion = 1;
+  console.log(massPaints)
 }
 
+function coordinatesPoint(point) {
+  let str = point.id.slice(0, point.id.indexOf("n"));
+  let col = point.id.slice(point.id.indexOf("n") + 1);
 
-function coordinatesPoint(point){
-    let str = point.id.slice(0,point.id.indexOf('n'))
-    let col = point.id.slice(point.id.indexOf('n')+1)
-
-    return {str,col}
+  return { str, col };
 }
 
-function checkPoints(str,col,player){
-
-}
-
+function checkPoints(str, col, player) {}
 
 let field2 = [
-    [{player: ''},{player: ''},{player: ''},{player: ''},{player: ''},{player: ''},{player: ''}],
-    [{player: ''},{player: ''},{player: 0},{player: ''},{player: ''},{player: ''},{player: ''}],
-    [{player: ''},{player: 0},{player: 1},{player: 0},{player: ''},{player: ''},{player: ''}],
-    [{player: ''},{player: ''},{player: 0},{player: ''},{player: ''},{player: ''},{player: ''}],
-    [{player: ''},{player: ''},{player: ''},{player: ''},{player: ''},{player: ''},{player: ''}],
-    [{player: ''},{player: ''},{player: ''},{player: ''},{player: ''},{player: ''},{player: ''}],
-    [{player: ''},{player: ''},{player: ''},{player: ''},{player: ''},{player: ''},{player: ''}]]
+  [
+    { player: "", statusChek: 0 },
+    { player: "", statusChek: 0 },
+    { player: "", statusChek: 0 },
+    { player: "", statusChek: 0 },
+    { player: "", statusChek: 0 },
+    { player: "", statusChek: 0 },
+    { player: "", statusChek: 0 },
+  ],
+  [
+    { player: "", statusChek: 0 },
+    { player: "", statusChek: 0 },
+    { player: "", statusChek: 0 },
+    { player: "", statusChek: 0 },
+    { player: "", statusChek: 0 },
+    { player: "", statusChek: 0 },
+    { player: "", statusChek: 0 },
+  ],
+  [
+    { player: "", statusChek: 0 },
+    { player: "", statusChek: 0 },
+    { player: "", statusChek: 0 },
+    { player: "", statusChek: 0 },
+    { player: "", statusChek: 0 },
+    { player: "", statusChek: 0 },
+    { player: "", statusChek: 0 },
+  ],
+  [
+    { player: "", statusChek: 0 },
+    { player: "", statusChek: 0 },
+    { player: "", statusChek: 0 },
+    { player: "", statusChek: 0 },
+    { player: "", statusChek: 0 },
+    { player: "", statusChek: 0 },
+    { player: "", statusChek: 0 },
+  ],
+  [
+    { player: "", statusChek: 0 },
+    { player: "", statusChek: 0 },
+    { player: "", statusChek: 0 },
+    { player: "", statusChek: 0 },
+    { player: "", statusChek: 0 },
+    { player: "", statusChek: 0 },
+    { player: "", statusChek: 0 },
+  ],
+  [
+    { player: "", statusChek: 0 },
+    { player: "", statusChek: 0 },
+    { player: "", statusChek: 0 },
+    { player: "", statusChek: 0 },
+    { player: "", statusChek: 0 },
+    { player: "", statusChek: 0 },
+    { player: "", statusChek: 0 },
+  ],
+  [
+    { player: "", statusChek: 0 },
+    { player: "", statusChek: 0 },
+    { player: "", statusChek: 0 },
+    { player: "", statusChek: 0 },
+    { player: "", statusChek: 0 },
+    { player: "", statusChek: 0 },
+    { player: "", statusChek: 0 },
+  ],
+];
 
-const squer = document.querySelector(".field2")
+const squer = document.querySelector(".field2");
 
-for (let i = 0; i < field2.length;i++) {
-    let line = document.createElement("div")
-    line.classList.add(".line")
-    for(let j = 0; j <field2[i].length;j++){
-        let btn = document.createElement('button')
-        if(field2[i][j].player !== ''){
-            if(field2[i][j].player){
-                btn.classList.add("firstPlayer")
-            }
-            else {btn.classList.add("secondPlayer")}
-        }
-        btn.id = `${i}n${j}`
-        line.append(btn)
-        body.append(line)
+for (let i = 0; i < field2.length; i++) {
+  let line = document.createElement("div");
+  line.classList.add(".line");
+  for (let j = 0; j < field2[i].length; j++) {
+    let btn = document.createElement("button");
+    if (field2[i][j].player !== "") {
+      if (field2[i][j].player) {
+        btn.classList.add("firstPlayer");
+      } else {
+        btn.classList.add("secondPlayer");
+      }
     }
+    btn.id = `${i}n${j}`;
+    line.append(btn);
+    body.append(line);
+  }
 }
 
-function check(str,col,motion){
 
-    let field2Copy = [...field2]
 
-    let positionsForChek = {str: -1}
+function giveAllPaints(str, col, player) {
+    console.log(str, col, player);
 
-    console.log(1+positionsForChek.str )
+  console.log(massPaints);
+  let peremForPush = false;
 
-    let massPointsForChek = []
-    // while(false){
-        // field2Copy[str-1][col-1] == motion ? massPointsForChek.push({str : str-1,col :col-1}): ''
-        // field2Copy[str-1][col] == motion ? massPointsForChek.push({str : str-1,col :col-1}): ''
-        // field2Copy[str-1][col+1] == motion ? massPointsForChek.push({str : str-1,col :col-1}): ''
-        // field2Copy[str][col-1] == motion ? massPointsForChek.push({str : str-1,col :col-1}): ''
-        // field2Copy[str][col+1] == motion ? massPointsForChek.push({str : str-1,col :col-1}): ''
-        // field2Copy[str+1][col-1] == motion ? massPointsForChek.push({str : str-1,col :col-1}): ''
-        // field2Copy[str+1][col] == motion ? massPointsForChek.push({str : str-1,col :col-1}): ''
-        // field2Copy[str+1][col+1] == motion ? massPointsForChek.push({str : str-1,col :col-1}): ''
-    // }
+  if (field2[str][col].statusChek == 1 || field2[str][col].player === player) {
+    
+    return;
+  }
 
-    console.log(field2Copy)
+  for (let i = 0; i < massPaints.length; i++) {
+    if (massPaints[i].str == str && massPaints[i].col == col) {
+      peremForPush = true;
+    }
+  }
+
+  if (peremForPush) {
+    massPaints.push({ str, col, player, statusChek: 1 });
+  }
+
+  field2[str][col].statusChek = 1;
+
+  if (
+    field2[str - 1][col - 1].player === player &&
+    field2[str - 1][col - 1].statusChek === 0
+  ) {
+    console.log(massPaints);
+    massPaints.push({ str: str - 1, col: col - 1, player, statusChek: 0 });
+  }
+  if (field2[str - 1][col].player === player && field2[str - 1][col].statusChek === 0) {
+    console.log(massPaints);
+    massPaints.push({ str: str - 1, col: col, player, statusChek: 0 });
+  }
+  if (
+    field2[str - 1][col + 1].player === player &&
+    field2[str - 1][col + 1].statusChek === 0 &&
+    checkOnPush(str - 1, col + 1, massPaints)
+  ) {
+    console.log(massPaints);
+    massPaints.push({ str: str - 1, col: col + 1, player, statusChek: 0 });
+    console.log(1);
+  }
+
+  if (
+    field2[str][col - 1].player === player &&
+    field2[str][col - 1].statusChek === 0 &&
+    checkOnPush(str, col - 1, massPaints)
+  ) {
+    console.log(massPaints);
+    massPaints.push({ str: str, col: col - 1, player, statusChek: 0 });
+  }
+  if (
+    field2[str][col + 1].player === player &&
+    field2[str][col + 1].statusChek === 0 &&
+    checkOnPush(str, col + 1, massPaints)
+  ) {
+    console.log(massPaints);
+    massPaints.push({ str: str, col: col + 1, player, statusChek: 0 });
+  }
+
+  if (
+    field2[str + 1][col - 1].player === player &&
+    field2[str + 1][col - 1].statusChek === 0 &&
+    checkOnPush(str + 1, col - 1, massPaints)
+  ) {
+    console.log(massPaints);
+    massPaints.push({ str: str + 1, col: col - 1, player, statusChek: 0 });
+  }
+  if (
+    field2[str + 1][col].player === player &&
+    field2[str + 1][col].statusChek === 0 &&
+    checkOnPush(str + 1, col, massPaints)
+  ) {
+    console.log(massPaints);
+    massPaints.push({ str: str + 1, col: col, player, statusChek: 0 });
+  }
+  if (
+    field2[str + 1][col + 1].player === player &&
+    field2[str + 1][col + 1].statusChek === 0 &&
+    checkOnPush(str + 1, col + 1, massPaints)
+  ) {
+    console.log(massPaints);
+
+    massPaints.push({ str: str + 1, col: col + 1, player, statusChek: 0 });
+    console.log(checkOnPush(str + 1, col + 1, massPaints));
+  }
+  console.log(massPaints);
+  for (let i = 0; i < massPaints.length; i++) {
+    if (massPaints[i].statusChek || massPaints[i].player !== player) {
+      continue;
+    }
+    massPaints[i].statusChek = 1;
+    return giveAllPaints(massPaints[i].str, massPaints[i].col, player);
+  }
 }
+
+function checkOnPush(strCheck, colCheck, massPaints) {
+  if (massPaints.length === 0) {
+    return;
+  }
+  for (let i = 0; i < massPaints.length; i++) {
+    if (
+      massPaints[i].str == strCheck &&
+      massPaints[i].col == colCheck &&
+      (massPaints[i].statusChek === 1 || massPaints[i].statusChek === 0)
+    ) {
+      if (massPaints[i].statusChek === 0) {
+        console.log("yes"); //написать цифры и покрасить точки
+      }
+
+      return false;
+    }
+  }
+  console.log("no");
+  return true;
+}
+
+
--- points/main2.js
+++ points/main2.js
@@ -1,6 +1,6 @@
 let body = document.querySelector(".field");
 
-let player = 1
+let player = 1;
 let massPaints = [];
 // for(let i = 0; i < 5;i++){
 // let str = document.createElement("div")
@@ -16,181 +16,177 @@ let massPaints = [];
 // })
 
 let mass = [
-	[
-		{ player: "", statusChek: 0 },
-		{ player: "", statusChek: 0 },
-		{ player: 0, statusChek: 0 },
-		{ player: 0, statusChek: 0 },
-		{ player: "", statusChek: 0 },
-	],
-	[
-		{ player: "", statusChek: 0 },
-		{ player: 0, statusChek: 0 },
-		{ player: 1, statusChek: 0 },
-		{ player: "", statusChek: 0 },
-		{ player: "", statusChek: 0 },
-	],
-	[
-		{ player: "", statusChek: 0 },
-		{ player: 1, statusChek: 0 },
-		{ player: 0, statusChek: 0 },
-		{ player: 1, statusChek: 0 },
-		{ player: "", statusChek: 0 },
-	],
-	[
-		{ player: "", statusChek: 0 },
-		{ player: "", statusChek: 0 },
-		{ player: 1, statusChek: 0 },
-		{ player: "", statusChek: 0 },
-		{ player: "", statusChek: 0 },
-	],
-	[
-		{ player: "", statusChek: 0 },
-		{ player: "", statusChek: 0 },
-		{ player: "", statusChek: 0 },
-		{ player: "", statusChek: 0 },
-		{ player: "", statusChek: 0 },
-	],
+  [
+    { player: "", statusChek: 0 },
+    { player: "", statusChek: 0 },
+    { player: 0, statusChek: 0 },
+    { player: 0, statusChek: 0 },
+    { player: "", statusChek: 0 },
+  ],
+  [
+    { player: "", statusChek: 0 },
+    { player: 0, statusChek: 0 },
+    { player: 1, statusChek: 0 },
+    { player: "", statusChek: 0 },
+    { player: "", statusChek: 0 },
+  ],
+  [
+    { player: "", statusChek: 0 },
+    { player: 1, statusChek: 0 },
+    { player: 0, statusChek: 0 },
+    { player: 1, statusChek: 0 },
+    { player: "", statusChek: 0 },
+  ],
+  [
+    { player: "", statusChek: 0 },
+    { player: "", statusChek: 0 },
+    { player: 1, statusChek: 0 },
+    { player: "", statusChek: 0 },
+    { player: "", statusChek: 0 },
+  ],
+  [
+    { player: "", statusChek: 0 },
+    { player: "", statusChek: 0 },
+    { player: "", statusChek: 0 },
+    { player: "", statusChek: 0 },
+    { player: "", statusChek: 0 },
+  ],
 ];
 
 function creatField() {
-	for (let i = 0; i < mass.length; i++) {
-		let str = document.createElement("div");
-		for (let j = 0; j < mass[i].length; j++) {
-			let btn = document.createElement("button");
-			if (mass[i][j].player) {
-				btn.classList.add("red");
-			}
-			if (mass[i][j].player === 0) {
-				btn.classList.add("blue");
-			}
+  for (let i = 0; i < mass.length; i++) {
+    let str = document.createElement("div");
+    for (let j = 0; j < mass[i].length; j++) {
+      let btn = document.createElement("button");
+      if (mass[i][j].player) {
+        btn.classList.add("red");
+      }
+      if (mass[i][j].player === 0) {
+        btn.classList.add("blue");
+      }
 
-			str.append(btn);
-		}
-		body.append(str);
-	}
+      str.append(btn);
+    }
+    body.append(str);
+  }
 }
 
 creatField();
 
-
-
-
 function giveAllPaints(str, col, player) {
+	console.log(str, col, player)
+  let newMass = [...mass];
+  console.log(massPaints);
+  let peremForPush = 0;
 
-	let newMass = [...mass];
-      console.log(massPaints)
-      let peremForPush = 0
+  if (mass[str][col].statusChek == 1 || mass[str][col].player === 0) {
+    return;
+  }
 
-	if (mass[str][col].statusChek == 1 || mass[str][col].player === 0) {
-		return;
-	}
+  for (let i = 0; i < massPaints.length; i++) {
+    if (massPaints[i].str == str && massPaints[i].col == col) {
+      peremForPush = 1;
+    }
+  }
 
-	for(let i = 0; i < massPaints.length;i++){
-	      if (massPaints[i].str == str && massPaints[i].col == col) {
-                  peremForPush = 1
-		}
-	}
+  if (!peremForPush) {
+    massPaints.push({ str, col, player, statusChek: 1 });
+  }
 
-      if (!peremForPush) {
-            massPaints.push({ str, col, player, statusChek: 1 });
-		}
+  mass[str][col].statusChek = 1;
 
-	mass[str][col].statusChek = 1;
+  if (
+    mass[str - 1][col - 1].player === 1 &&
+    mass[str - 1][col - 1].statusChek === 0
+  ) {
+    console.log(massPaints);
+    massPaints.push({ str: str - 1, col: col - 1, player, statusChek: 0 });
+  }
+  if (mass[str - 1][col].player === 1 && mass[str - 1][col].statusChek === 0) {
+    console.log(massPaints);
+    massPaints.push({ str: str - 1, col: col, player, statusChek: 0 });
+  }
+  if (
+    mass[str - 1][col + 1].player === 1 &&
+    mass[str - 1][col + 1].statusChek === 0 &&
+    checkOnPush(str - 1, col + 1, massPaints)
+  ) {
+    console.log(massPaints);
+    massPaints.push({ str: str - 1, col: col + 1, player, statusChek: 0 });
+    console.log(1);
+  }
 
-	if (
-		mass[str - 1][col - 1].player === 1 &&
-		mass[str - 1][col - 1].statusChek === 0
-	) {
-            console.log(massPaints);
-		massPaints.push({ str: str - 1, col: col - 1, player, statusChek: 0 });
-	}
-	if (mass[str - 1][col].player === 1 && mass[str - 1][col].statusChek === 0) {
-            console.log(massPaints);
-		massPaints.push({ str: str - 1, col: col, player, statusChek: 0 });
-	}
-	if (
-		mass[str - 1][col + 1].player === 1 &&
-		mass[str - 1][col + 1].statusChek === 0 &&
-		checkOnPush(str - 1, col + 1, massPaints)
-	) {
-		console.log(massPaints);
-		massPaints.push({ str: str - 1, col: col + 1, player, statusChek: 0 });
-		console.log(1);
-	}
+  if (
+    mass[str][col - 1].player === 1 &&
+    mass[str][col - 1].statusChek === 0 &&
+    checkOnPush(str, col - 1, massPaints)
+  ) {
+    console.log(massPaints);
+    massPaints.push({ str: str, col: col - 1, player, statusChek: 0 });
+  }
+  if (
+    mass[str][col + 1].player === 1 &&
+    mass[str][col + 1].statusChek === 0 &&
+    checkOnPush(str, col + 1, massPaints)
+  ) {
+    console.log(massPaints);
+    massPaints.push({ str: str, col: col + 1, player, statusChek: 0 });
+  }
 
-	if (
-		mass[str][col - 1].player === 1 &&
-		mass[str][col - 1].statusChek === 0 &&
-		checkOnPush(str, col - 1, massPaints)
-	) {
-		console.log(massPaints);
-		massPaints.push({ str: str, col: col - 1, player, statusChek: 0 });
-	}
-	if (
-		mass[str][col + 1].player === 1 &&
-		mass[str][col + 1].statusChek === 0 &&
-		checkOnPush(str, col + 1, massPaints)
-	) {
-		console.log(massPaints);
-		massPaints.push({ str: str, col: col + 1, player, statusChek: 0 });
-	}
+  if (
+    mass[str + 1][col - 1].player === 1 &&
+    mass[str + 1][col - 1].statusChek === 0 &&
+    checkOnPush(str + 1, col - 1, massPaints)
+  ) {
+    console.log(massPaints);
+    massPaints.push({ str: str + 1, col: col - 1, player, statusChek: 0 });
+  }
+  if (
+    mass[str + 1][col].player === 1 &&
+    mass[str + 1][col].statusChek === 0 &&
+    checkOnPush(str + 1, col, massPaints)
+  ) {
+    console.log(massPaints);
+    massPaints.push({ str: str + 1, col: col, player, statusChek: 0 });
+  }
+  if (
+    mass[str + 1][col + 1].player === 1 &&
+    mass[str + 1][col + 1].statusChek === 0 &&
+    checkOnPush(str + 1, col + 1, massPaints)
+  ) {
+    console.log(massPaints);
 
-	if (
-		mass[str + 1][col - 1].player === 1 &&
-		mass[str + 1][col - 1].statusChek === 0 &&
-		checkOnPush(str + 1, col - 1, massPaints)
-	) {
-		console.log(massPaints);
-		massPaints.push({ str: str + 1, col: col - 1, player, statusChek: 0 });
-	}
-	if (
-		mass[str + 1][col].player === 1 &&
-		mass[str + 1][col].statusChek === 0 &&
-		checkOnPush(str + 1, col, massPaints)
-	) {
-		console.log(massPaints);
-		massPaints.push({ str: str + 1, col: col, player, statusChek: 0 });
-	}
-	if (
-		mass[str + 1][col + 1].player === 1 &&
-		mass[str + 1][col + 1].statusChek === 0 &&
-		checkOnPush(str + 1, col + 1, massPaints)
-	) {
-		console.log(massPaints);
-
-		massPaints.push({ str: str + 1, col: col + 1, player, statusChek: 0 });
-		console.log(checkOnPush(str + 1, col + 1, massPaints));
-	}
-	console.log(massPaints);
-	for (let i = 0; i < massPaints.length; i++) {
-		if (massPaints[i].statusChek || massPaints[i].player === 0) {
-			continue;
-		}
-		massPaints[i].statusChek = 1;
-		return giveAllPaints(massPaints[i].str, massPaints[i].col, player);
-	}
+    massPaints.push({ str: str + 1, col: col + 1, player, statusChek: 0 });
+    console.log(checkOnPush(str + 1, col + 1, massPaints));
+  }
+  console.log(massPaints);
+  for (let i = 0; i < massPaints.length; i++) {
+    if (massPaints[i].statusChek || massPaints[i].player === 0) {
+      continue;
+    }
+    massPaints[i].statusChek = 1;
+    return giveAllPaints(massPaints[i].str, massPaints[i].col, player);
+  }
 }
 giveAllPaints(2, 1, player);
 
-
 function checkOnPush(strCheck, colCheck, massPaints) {
-	if (massPaints.length === 0) {
-		return;
-	}
-	for (let i = 0; i <  massPaints.length; i++) {
-		if (
-			massPaints[i].str == strCheck &&
-			massPaints[i].col == colCheck &&
-			(massPaints[i].statusChek === 1 || massPaints[i].statusChek === 0)
-		) {
-                  if (massPaints[i].statusChek === 0) {
-                        console.log("yes");//написать цифры и покрасить точки
-			      }
+  if (massPaints.length === 0) {
+    return;
+  }
+  for (let i = 0; i < massPaints.length; i++) {
+    if (
+      massPaints[i].str == strCheck &&
+      massPaints[i].col == colCheck &&
+      (massPaints[i].statusChek === 1 || massPaints[i].statusChek === 0)
+    ) {
+      if (massPaints[i].statusChek === 0) {
+        console.log("yes"); //написать цифры и покрасить точки
+      }
 
-			return false;
-		}
-	}
-      console.log("no")
-	return true;
+      return false;
+    }
+  }
+  console.log("no");
+  return true;
 }
